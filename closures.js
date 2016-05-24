67987145fb8dde66c655d0b7fc4c8a16bb020f14
mckmillions <zachmckimmins@gmail.com>
2016-05-24 15:26:59 -0500

    cleaned up questions

--- closures.js
+++ closures.js
@@ -65,30 +65,6 @@ var callFriend = function(){
 //////////////////PROBLEM 4////////////////////
 
 
-// Inside the function called counterFactory
-// return two functions that implement up/down counter.
-// The first function is called inc, this function is responsible for incrementing the value once
-// The second function is called dec, this function is responsible for decrementing the value by one
-// You will need to use the module pattern to achieve this.
-
-function counterFactory(value) {
-
-  // Code here.
-
-
-  return {
-  }
-}
-
-
-counter = counterFactory(10);
-
-
-
-
-//////////////////PROBLEM 5////////////////////
-
-
 // Inside the motivation function create another function called message that will return 'You're doing awesome, keep it up firstname lastname.'
 
   function motivation(firstname, lastname){
@@ -106,37 +82,7 @@ counter = counterFactory(10);
   motivation('Billy', 'Bob'); // 'Your doing awesome keep it up Billy Bob
 
 
-
-//////////////////PROBLEM 6////////////////////
-
-// Inside the return create a publicMethod property that is a function that invokes privateMethod. After you create the privateMethod
-// Invoke it by calling module.publicMethod(); outside the module scope
-
-  var module = (function() {
-    var person = {
-      name: "phillip",
-      age: 29,
-      location: 'Utah'
-    };
-
-    var privateMethod = function(){
-      return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
-    };
-
-    // Anything that is being returned is made public and can be invoked from outside our lexical scope
-
-    return {
-      // Code here.
-    };
-
-  })();
-
-//Uncomment this after you create your public method
-//   module.publicMethod();
-
-
-
-//////////////////PROBLEM 7////////////////////
+//////////////////PROBLEM 5////////////////////
 // Here we have a for loop that will iterate as long as i is less than or equal to 5. What we need to do is console.log(i)
 // So that it logs ( 0 then 1 then 2 then 3, etc). Run this code in your console to see what the output is.
 
@@ -159,7 +105,7 @@ timeOutCounter();
 
 
 
-//////////////////PROBLEM 8////////////////////
+//////////////////PROBLEM 6////////////////////
 
 var funcArray = [];
 
